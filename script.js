cccb0954fe6d21769ce9a3a06722d57bd7c9baf9
Connor Neill <cwmn1975@comcast.net>
2025-05-23 15:44:38 -0400

    Added getComputerChoice function
    Added function, getComputerChoice, to generate a random integer of 0, 1, or 2, and return rock, paper, or scissors as appropriate.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    
+    if (num === 1) {
+        return "rock";
+    } else if (num === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
