ed3ccdfa324f95032d2320507d3f9e2ea4943e7d
Connor Neill <cwmn1975@comcast.net>
2025-05-23 16:31:36 -0400

    Writing playRound function
    Filled out playRound function, which takes the player's choice and computer's choice and then determines who won the game, announces it, and increments scores.
    There is currently a bug where the player and computer choices are the same every round.

--- script.js
+++ script.js
@@ -18,5 +18,19 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
+    console.log(`Player chose ${humanChoice}. Computer chose ${computerChoice}.`);
     
+    if (((humanChoice === 'rock') && (computerChoice === 'scissors')) || ((humanChoice === 'paper') && (computerChoice === 'rock')) || ((humanChoice === 'scissors') && (computerChoice === 'paper'))) {
+        console.log("Player wins!");
+        humanScore++;
+    } else if (((humanChoice === 'rock') && (computerChoice === 'paper')) || ((humanChoice === 'paper') && (computerChoice === 'scissors')) || ((humanChoice === 'scissors') && (computerChoice === 'rock'))) {
+        console.log("Computer wins!");
+        computerScore++;
+    } else {
+        console.log("Tie!");
+    }
+
+    console.log(`Player score: ${humanScore}| Computer Score: ${computerScore}`);
 }
+
+playRound(getHumanChoice(), getComputerChoice());
