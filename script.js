f3ccd6fe82f2c327d89f77f3f40ef7fc7c0897ec
Connor Neill <cwmn1975@comcast.net>
2025-07-16 17:58:48 -0400

    Moving old functions to bottom, documentation
    Moved old console-based functions to bottom of script.
    Adjusted spacing and added documentation.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-/* Yes, I could absolutely make these variables not global for better access, but 
-I didn't want to refactor my functions to do it. */
-// Score variables
+/* Score variables */
 let humanScore = 0;
 let computerScore = 0;
 
@@ -13,7 +11,6 @@ btn.addEventListener("click", (event) => {
 });
 
 /* Generate computer's game choice */
-/* Deprecated with addition of UI */
 function getComputerChoice() {
 
     let num = Math.floor(Math.random() * 3);
@@ -27,12 +24,6 @@ function getComputerChoice() {
     }
 }
 
-/* Generate player's game choice, and set it to all lower case */
-/* Deprecated with addition of UI */
-function getHumanChoice() {
-    return prompt("Choose rock, paper, or scissors.").toLowerCase();
-}
-
 /* Plays one round of rock, paper, scissors, incrementing correct score value */
 function playRound(humanChoice, computerChoice) {
 
@@ -41,47 +32,69 @@ function playRound(humanChoice, computerChoice) {
     const scoreDisplay = document.querySelector('#score');
     const matchResults = document.querySelector('#matchResults');
 
-    console.log(`Player chose ${humanChoice}. Computer chose ${computerChoice}.`);
+    // Clear match results from previous match
+    matchResults.textContent = '';
+
+    // console.log(`Player chose ${humanChoice}. Computer chose ${computerChoice}.`);
     choicesDisplay.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}.`;
     
-    if (((humanChoice === 'rock') && (computerChoice === 'scissors')) || ((humanChoice === 'paper') && (computerChoice === 'rock')) || ((humanChoice === 'scissors') && (computerChoice === 'paper'))) {
-        console.log("Player wins!");
+    if (((humanChoice === 'rock') && (computerChoice === 'scissors')) || 
+        ((humanChoice === 'paper') && (computerChoice === 'rock')) || 
+        ((humanChoice === 'scissors') && (computerChoice === 'paper'))) {
+        // console.log("Player wins!");
         roundResults.textContent = 'Player wins!';
         humanScore++;
-    } else if (((humanChoice === 'rock') && (computerChoice === 'paper')) || ((humanChoice === 'paper') && (computerChoice === 'scissors')) || ((humanChoice === 'scissors') && (computerChoice === 'rock'))) {
-        console.log("Computer wins!");
+    } else if (((humanChoice === 'rock') && (computerChoice === 'paper')) || 
+               ((humanChoice === 'paper') && (computerChoice === 'scissors')) || 
+               ((humanChoice === 'scissors') && (computerChoice === 'rock'))) {
+        // console.log("Computer wins!");
         roundResults.textContent = 'Computer wins!'
         computerScore++;
     } else {
-        console.log("Tie!");
+        // console.log("Tie!");
         roundResults.textContent = 'Tie!';
     }
 
     scoreDisplay.textContent = `Player score: ${humanScore} | Computer Score: ${computerScore}`;
+
+    // Check for victory threshold
+    if (humanScore >= 5) {
+        matchResults.textContent = 'Congratulations! You win!'
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore >= 5) {
+        matchResults.textContent = 'Better luck next time! Computer wins.'
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
+
+/* Generate player's game choice, and set it to all lower case */
+/* Deprecated with addition of UI */
+function getHumanChoice() {
+    return prompt("Choose rock, paper, or scissors.").toLowerCase();
 }
 
 /* Plays a 5-round game of rock, paper, scissors in the console. Sets score values to 0 at the start of each game. */
-// function playGame() {
+function playGame() {
 
-//     humanScore = 0;
-//     computerScore = 0;
+    humanScore = 0;
+    computerScore = 0;
 
-//     for (i = 1; i < 6; i++) {
-//         console.log(`Round ${i}:`);
+    for (i = 1; i < 6; i++) {
+        console.log(`Round ${i}:`);
 
-//         let humanSelection = getHumanChoice();
-//         let computerSelection = getComputerChoice();
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
 
-//         playRound(humanSelection, computerSelection);
-//     }
+        playRound(humanSelection, computerSelection);
+    }
 
-//     if (humanScore > computerScore) {
-//         console.log("Player wins the game!");
-//     } else if (humanScore < computerScore) {
-//         console.log("Computer wins the game!");
-//     } else {
-//         console.log("Game is a tie!");
-//     }
-// }
-
-// playGame();
+    if (humanScore > computerScore) {
+        console.log("Player wins the game!");
+    } else if (humanScore < computerScore) {
+        console.log("Computer wins the game!");
+    } else {
+        console.log("Game is a tie!");
+    }
+}
